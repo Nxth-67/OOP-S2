6b20876db0fcac5712413163d6eaa49afaa81c3e
SOPHANITH <yoongsophanith18@gmail.com>
2026-04-02 23:14:12 +0700

    Age

--- Algorithm.java
+++ Algorithm.java
@@ -1,5 +1,6 @@
 public class Algorithm {
     public static void main(String[] args) {
-      System.out.println("Bonjour!"); 
+      System.out.println("Bonjour!");
+      System.out.println("How are you?"); 
     }
 }
